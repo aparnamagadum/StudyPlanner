dc9b0c1a56f45aa6702d78e334662ee42df9bcc0
aparnamagadum <aparnamagadum2@gmail.com>
2024-07-13 12:15:22 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LoremTextGenerator from './LoremTextGenerator'
+import StudyPlanner from './studyPlanner'
 
 function App() {
 
   return (
     <>
-    <div className='container'>
-      <LoremTextGenerator />
-      </div>
+      <StudyPlanner />
     </>
   )
 }
